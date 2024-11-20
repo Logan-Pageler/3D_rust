977b69255615e63cdfbc08f919262baed5f958c3
Logan Pageler <pagelerjake@arizona.edu>
2024-11-19 19:33:49 -0700

    rainbow triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::EventLoop,
@@ -9,7 +10,7 @@ use winit::window::Window;
 
 // Set up vertices and vertex buffers
 #[repr(C)]
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
     position: [f32; 3],
     color: [f32; 3],
@@ -21,6 +22,28 @@ const VERTICES: &[Vertex] = &[
     Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
 ];
 
+impl Vertex {
+    // establish vertex description
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        // describe what the buffer looks like in memory
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,  // defined the width of the vertex
+            step_mode: wgpu::VertexStepMode::Vertex,  // each element is per vertex
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,  // offset to attribute start
+                    shader_location: 0,  // get the position field
+                    format: wgpu::VertexFormat::Float32x3,  // size of attribute
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,  // get the color field
+                    format: wgpu::VertexFormat::Float32x3,
+                }
+            ]
+        }
+    }
+}
 
 
 // structure to store the sate of the window/frame
@@ -33,10 +56,12 @@ struct State<'a> {
     // describe how we render things
     render_pipeline: wgpu::RenderPipeline,
     // store all the vertices we want to render
+    vertex_buffer: wgpu::Buffer,
     // The window must be declared after the surface so
     // it gets dropped after it as the surface contains
     // unsafe references to the window's resources.
     window: &'a Window,
+    num_vertices: u32,
 }
 
 impl<'a> State<'a> {
@@ -111,7 +136,9 @@ impl<'a> State<'a> {
             vertex: wgpu::VertexState { // Specify that we use the vertex function from shader.wgsl
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[],
+                buffers: &[
+                    Vertex::desc(),
+                ],
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
             },
             fragment: Some(wgpu::FragmentState { // Specify that we use the fragment vertex function from shader.wgsl
@@ -145,6 +172,17 @@ impl<'a> State<'a> {
             multiview: None, // we also wont be using array textures
             cache: None, // we dont need caching either
         });
+        
+        // set up the vertex buffer
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+        
+        let num_vertices = VERTICES.len() as u32;
 
         Self {
             window,
@@ -154,6 +192,8 @@ impl<'a> State<'a> {
             config,
             size,
             render_pipeline,
+            vertex_buffer,
+            num_vertices,
         }
     }
 
@@ -216,8 +256,9 @@ impl<'a> State<'a> {
             });
 
             // Use our pipeline we defined
-            render_pass.set_pipeline(&self.render_pipeline); // 2.
-            render_pass.draw(0..3, 0..1); // 3.
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.draw(0..self.num_vertices, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
